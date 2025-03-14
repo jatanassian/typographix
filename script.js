29c2b62fa8a28ec7aca5248ebea9d303c546a7e6
Julien Atanassian <julien.atanassian@gmail.com>
2025-03-14 00:14:50 -0400

    add navigation menu

--- script.js
+++ script.js
@@ -31,3 +31,17 @@ if (currentThemeFromLocaleStorage) {
   document.documentElement.setAttribute('data-theme', currentThemeFromLocaleStorage);
   currentThemeFromLocaleStorage === 'dark' ? setDarkMode() : setLightMode();
 }
+
+// Navigation
+const nav = document.getElementById('nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenu() {
+  nav.classList.toggle('active');
+  menuIcon.classList.toggle('active');
+}
+
+function hideMenu() {
+  nav.classList.remove('active');
+  menuIcon.classList.remove('active');
+}
